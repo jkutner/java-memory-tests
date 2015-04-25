4f30b3f47d5744670f1f87df960c8e10136b3d53
Joe Kutner <jpkutner@gmail.com>
2015-04-24 22:53:43 -0500

    increased loop sleep

--- loop.sh
+++ loop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while :; do ./run.sh $@ && echo "" && echo "sleeping... $(date)" && echo ""; sleep 60; done
+while :; do ./run.sh $@ && echo "" && echo "sleeping... $(date)" && echo ""; sleep 90; done
